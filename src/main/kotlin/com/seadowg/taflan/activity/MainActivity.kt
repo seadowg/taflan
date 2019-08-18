e41181ed84eef884424b7cebccdda89fbebca9c6
Callum Stott <callum@seadowg.com>
2019-08-18 14:27:43 +0100

    Remove open source license plugin

--- src/main/kotlin/com/seadowg/taflan/activity/MainActivity.kt
+++ src/main/kotlin/com/seadowg/taflan/activity/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.seadowg.taflan.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.seadowg.taflan.R
 
 class MainActivity : TaflanActivity() {
@@ -14,21 +10,5 @@ class MainActivity : TaflanActivity() {
         setContentView(R.layout.main)
 
         setupToolbar(getString(R.string.app_title))
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.app_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.open_source_licenses -> {
-                startActivity(Intent(this, OssLicensesMenuActivity::class.java))
-                true
-            }
-
-            else -> false
-        }
     }
 }
